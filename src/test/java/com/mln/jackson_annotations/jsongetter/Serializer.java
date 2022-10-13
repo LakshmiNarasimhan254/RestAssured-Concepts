8169748d8e3c1ba511c7bddbdf87079c88588da3
laksh <lakshminarasimhan254@gmail.com>
2022-10-13 11:11:35 -0400

    Jackson Annotation series-JsonGetterAnnotation

---
+++ src/test/java/com/mln/jackson_annotations/jsongetter/Serializer.java
@@ -0,0 +1,30 @@
+package com.mln.jackson_annotations.jsongetter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class Serializer {
+     Student student = new Student();
+     Faker faker = new Faker();
+
+    public Student setStudent() {
+      student.setFirst_Name(faker.name().firstName());
+      student.setFirst_Name(faker.name().lastName());
+      student.setAge(faker.number().numberBetween(10,34));
+      student.setId(faker.idNumber().valid());
+      student.setSubjectId(faker.number().numberBetween(1,3));
+      student.setSkills(Arrays.asList(faker.programmingLanguage().name(),faker.programmingLanguage().name(),faker.programmingLanguage().name()));
+      return student;
+    }
+
+    @Test
+    public void jsonGetterSerializer() throws JsonProcessingException {
+        ObjectMapper mapper =  new ObjectMapper();
+        String serializedStudent= mapper.writerWithDefaultPrettyPrinter().writeValueAsString(setStudent());
+        System.out.println(serializedStudent);
+    }
+}
